3fd37a24c9631440944dd82d4baf9631eb5f1470
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-09 20:22:04 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[selectedTab, setSelectedTab] = useState('Tab 1')
   return (
     <div>
         {/* Do not remove the main div */}
+        <ul>
+          <li onClick={()=>setSelectedTab('Tab 1')} >Tab 1</li>
+          <li onClick={()=>setSelectedTab('Tab 2')} >Tab 2</li>
+          <li onClick={()=>setSelectedTab('Tab 3')} >Tab 3</li>
+        </ul>
+        <p>This is the content of {selectedTab}</p>
     </div>
   )
 }
